5b19bde0154486be77b8ad5089ba47a28d9c6b99
htoomyatnyinyi42nn <htoomyatnyinyi42.nn@gmail.com>
2026-01-17 10:11:17 +0700

    revise logic to match with server 4.0_server

--- services/api/baseApi.ts
+++ services/api/baseApi.ts
@@ -5,8 +5,10 @@ import { Platform } from "react-native";
 export const BASE_URL =
   Platform.OS === "android"
     ? "http://10.0.2.2:8080"
-    : "http://192.168.1.143:8080";
-// const BASE_URL = "http://192.168.1.XX:8080"; // Replace with your IP
+    : "http://192.168.1.143:8080"; // <--- UPDATE THIS IP to your server's current local IP
+// const BASE_URL = "http://192.168.1.67:8080";
+// const BASE_URL = "http://192.168.137.136:8080";
+// const BASE_URL = "http://localhost:8080";
 const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
